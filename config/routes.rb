0c507fdf26d3a42beaa68486c61d4dd7f3ac24d2
Alan Cumiskey <zilk@campus.ie>
2015-01-12 16:27:22 +0000

    Addeed the about pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Stukdo::Application.routes.draw do
   # You can have the root of your site routed with "root"
  root 'pages#home'
 
+ get 'about' => "pages#about"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
