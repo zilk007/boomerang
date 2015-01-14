9bc5d7f161fa01590bfa7102d9fd4835aa9ea3ec
Alan Cumiskey <zilk@campus.ie>
2015-01-14 11:31:43 +0000

    Created tasks scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Stukdo::Application.routes.draw do
+  resources :tasks
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
