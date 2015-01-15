454eae3e002b47de44653cd14bd6762107447bc3
Alan Cumiskey <zilk@campus.ie>
2015-01-15 00:36:42 +0000

    Added production noded

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,11 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.precompile += ["fontawesome-webfont.ttf",
+                             "fontawesome-webfont.eot",
+                             "fontawesome-webfont.svg",
+                             "fontawesome-webfont.woff"]
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,6 @@
 Stukdo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.assets.precompile += %w( 
-    *.svg *.eot *.woff *.ttf *.png *.jpg *.jpeg *.gif
-  )
-
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
