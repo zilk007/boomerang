04ce80d865ca98db949b624d5a5fa4d143a64bdf
Alan Cumiskey <zilk@campus.ie>
2015-01-15 16:22:36 +0000

    Status change

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,14 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
-ruby config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
 module Stukdo
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
